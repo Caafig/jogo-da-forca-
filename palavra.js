a36db74e6ae7b45a043ac31652cc8ac71afa9b71
Caafig <carolfig72@gmail.com>
2024-12-01 15:31:19 -0300

    Create palavra.js

---
+++ palavra.js
@@ -0,0 +1,17 @@
+const palavras = [{ word: "rio", clue: "Corpo de água que corre para o mar" },
+{ word: "montanha", clue: "Elevação alta de terra" },
+{ word: "livro", clue: "Objeto que contém páginas com textos" },
+{ word: "sol", clue: "Estrela que ilumina o dia" },
+{ word: "cachorro", clue: "Animal de estimação que late" },
+{ word: "casa", clue: "Lugar onde as pessoas moram" },
+{ word: "carro", clue: "Veículo com quatro rodas" },
+{ word: "janela", clue: "Abertura na parede para ver fora" },
+{ word: "flor", clue: "Parte colorida de uma planta" },
+{ word: "praia", clue: "Área de areia perto do mar" },]
+
+
+
+export function getWord() {
+  const index = Math.floor(Math.random() * palavras.length);
+  return palavras[index];
+}
